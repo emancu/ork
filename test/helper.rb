95248b276d0526bd7913c0043fbac2d10d9abbeb
eMancu <emiliano.mancuso@gmail.com>
2013-07-09 14:30:34 -0300

    Refactor helper
    Load the config before the whole environment.
    This will prevent unnecessary output and conflicts with Riak.
    [skip ci]

--- test/helper.rb
+++ test/helper.rb
@@ -1,5 +1,10 @@
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../lib"))
 
+unless File.exist?(File.expand_path("../test_riak_server.toml", __FILE__))
+  puts "Specify the location of your Riak installation in test/test_riak_server.toml"
+  exit(1)
+end
+
 require 'coveralls'
 Coveralls.wear!
 
@@ -12,34 +17,33 @@ end
 
 require "rubygems"
 require "protest"
-require "ork"
+require 'toml'
 require 'riak/test_server'
+require "ork"
 
 Riak.disable_list_keys_warnings = true
 
 def test_server
-  $test_server ||= begin
-     require 'toml'
-     path = File.expand_path("../test_riak_server.toml", __FILE__)
-     config = TOML.load_file(path, symbolize_keys: true)
+  $server ||= begin
+                path = File.expand_path("../test_riak_server.toml", __FILE__)
+                config = TOML.load_file(path, symbolize_keys: true)
+
+                server = Riak::TestServer.create(root: config[:root],
+                                                 source: config[:source],
+                                                 min_port: config[:min_port])
 
 
-     server = Riak::TestServer.create(root: config[:root],
-                                      source: config[:source],
-                                      min_port: config[:min_port])
+                Ork.connect(:test, {
+                  http_port: server.http_port,
+                  pb_port:   server.pb_port
+                })
 
-
-     Ork.connect(:test, {
-      http_port: server.http_port,
-      pb_port:   server.pb_port
-     })
-
-     server
-   rescue => e
-     puts ("Can't run Ork tests without the test server.\n" +
-             "Specify the location of your Riak installation in test/test_riak_server.toml\n" +
-             e.inspect)
-   end
+                server
+              rescue => e
+                puts "Can't run Ork tests without the test server."
+                puts e.inspect
+                exit(1)
+              end
 end
 
 def flush_db!
