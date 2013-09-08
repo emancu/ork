d6a03b47df81eb85b50a23254f5683bb8c335934
eMancu <emiliano.mancuso@gmail.com>
2013-09-08 05:56:39 -0300

    Add Embedd Errors

--- lib/ork/errors.rb
+++ lib/ork/errors.rb
@@ -2,4 +2,7 @@ module Ork
   class Error < StandardError; end
   class IndexNotFound < RuntimeError; end
   class UniqueIndexViolation < RuntimeError; end
+
+  class NotAnEmbeddableObject < RuntimeError; end
+  class ParentMissing < RuntimeError; end
 end
