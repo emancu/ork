f7aa63b4d5646aad2a08e849126d7883d1b4d9b1
eMancu <emiliano.mancuso@gmail.com>
2013-10-05 11:22:51 -0300

    Remove TestServer since the new Riak broke it.
    Also it was so slow that is better to start risk and run tests.

--- test/helper.rb
+++ test/helper.rb
@@ -1,10 +1,5 @@
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../lib"))
 
-unless File.exist?(File.expand_path("../test_riak_server.toml", __FILE__))
-  puts "Specify the location of your Riak installation in test/test_riak_server.toml"
-  exit(1)
-end
-
 require 'coveralls'
 Coveralls.wear!
 
@@ -17,35 +12,10 @@ end
 
 require "rubygems"
 require "protest"
-require 'toml'
-require 'riak/test_server'
 require "ork"
 
 Riak.disable_list_keys_warnings = true
 Protest.report_with(:progress)
-
-def test_server
-  $server ||= begin
-                path = File.expand_path("../test_riak_server.toml", __FILE__)
-                config = TOML.load_file(path, symbolize_keys: true)
-
-                server = Riak::TestServer.create(root: config[:root],
-                                                 source: config[:source],
-                                                 min_port: config[:min_port])
-
-
-                Ork.connect(:test, {
-                  http_port: server.http_port,
-                  pb_port:   server.pb_port
-                })
-
-                server
-              rescue => e
-                puts "Can't run Ork tests without the test server."
-                puts e.inspect
-                exit(1)
-              end
-end
 
 def randomize_bucket_name(klass)
   klass.bucket_name= [
